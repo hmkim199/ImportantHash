7c5ce6200be4266945b4e175d44df1e0e02c6174
[3기-1반] 김혜민 <hmkim199@naver.com>
2022-02-22 12:29:34 +0000

    Implement video, keyword, frequency, importance models

---
+++ backend/apps/script/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from backend.apps.video.models import Video
+
+# Create your models here.
+class Script():
+    """
+    Script 모델
+    """
+
+    video = models.ForeignKey(
+        Video,
+        verbose_name="동영상 id",
+        on_delete=models.CASCADE,
+        related_name="video_id",
+    )
+
+    timestamp = models.TimeField(
+        verbose_name="타임스탬프",
+    )
+
+    content = models.TextField(
+        verbose_name="시간에 따른 스크립트 내용",
+    )
+
+    class Meta:
+        verbose_name_plural = "스크립트"
+        db_table = "script"
+
+    def __str__(self) -> str:
+        self.content
---
+++ backend/apps/video/models.py
@@ -0,0 +1,33 @@
+from gc import DEBUG_COLLECTABLE
+from imp import source_from_cache
+from django.db import models
+
+# Create your models here.
+class Video():
+    """
+    Video 모델
+    """
+
+    # user_id = models.ForeignKey(
+    #     User,
+    #     verbose_name="사용자",
+    #     on_delete=models.CASCADE,
+    #     related_name="video_user",
+    # )
+
+    source = models.URLField(
+        verbose_name="동영상 URL",
+        max_length=255
+    )
+
+    title = models.CharField(
+        verbose_name="동영상 제목",
+        max_length=255,
+    )
+
+    class Meta:
+        verbose_name_plural = "동영상"
+        db_table = "video"
+
+    def __str__(self) -> str:
+        self.title

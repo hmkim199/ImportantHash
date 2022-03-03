bdafc9c92e9292cf42b7bdcf34a4d27a0b729608
[3기-2반] 진병수 <castela0119@gmail.com>
2022-03-03 16:19:27 +0900

    feat:complete_userapp_swagger

--- backend/apps/user_app/views.py
+++ backend/apps/user_app/views.py
@@ -1,22 +1,10 @@
 from .serializers import (RegistrationSerializer, MyTokenObtainPairSerializer, MyTokenVerifySerializer, MyTokenBlacklistSerializer, MyTokenRefreshSerializer)
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenVerifyView, TokenRefreshView, TokenBlacklistView)
 
-from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from django.utils.translation import gettext_lazy as _
-from rest_framework import exceptions, status
-
-class AuthenticationFailed(exceptions.AuthenticationFailed):
-    pass
-class InvalidToken(AuthenticationFailed):
-    status_code = status.HTTP_401_UNAUTHORIZED
-    default_detail = _('Token is invalid or expired')
-    default_code = 'token_not_valid'
 class RegisterView(APIView):
 
     @swagger_auto_schema(
@@ -56,7 +44,6 @@ class RegisterView(APIView):
                 return Response(data, status=status.HTTP_409_CONFLICT)
                 
         
-            
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -68,14 +55,19 @@ class MyTokenObtainPairView(TokenObtainPairView):
         operation_description="로그인시 토큰발급",
         responses={ # can use schema or text 
                     200: '로그인 성공', 
-                    401: '없는회원이거나 비밀번호틀림' }
+                    409: '없는회원이거나 비밀번호틀림' }
     )
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
-class MyTokenVerifyView(TokenVerifyView, InvalidToken):
+        try:
+            serializer.is_valid(raise_exception=True)
+            return Response(data=serializer.validated_data, status=status.HTTP_200_OK)
+
+        except:
+            return Response("not user or wrong password!", status.HTTP_409_CONFLICT)
+
+class MyTokenVerifyView(TokenVerifyView):
     serializer_class = MyTokenVerifySerializer
 
     @swagger_auto_schema(
@@ -85,16 +77,19 @@ class MyTokenVerifyView(TokenVerifyView, InvalidToken):
         operation_description="토큰 유효확인",
         responses={ # can use schema or text 
                     200: '토큰 유효', 
-                    400: '토큰 무효' }
+                    401: '토큰 무효' }
     )
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            return Response(serializer.validated_data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            serializer.is_valid(raise_exception=True)
+            return Response(data=serializer.validated_data, status=status.HTTP_200_OK)
 
-class MyTokenRefreshView(TokenRefreshView, InvalidToken):
+        except:
+            return Response("Token is invalid or expired", status.HTTP_401_UNAUTHORIZED)
+
+class MyTokenRefreshView(TokenRefreshView):
     serializer_class = MyTokenRefreshSerializer
 
     @swagger_auto_schema(
@@ -104,16 +99,12 @@ class MyTokenRefreshView(TokenRefreshView, InvalidToken):
         operation_description="토큰 재발행",
         responses={ # can use schema or text 
                     200: '토큰 재발행 성공', 
-                    400: '없는 토큰' }
+                    401: '없는 토큰' }
     )
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            return Response(serializer.validated_data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer, status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_200_OK)
 
-class MyTokenBlacklistView(TokenBlacklistView, InvalidToken):
+class MyTokenBlacklistView(TokenBlacklistView):
     serializer_class = MyTokenBlacklistSerializer
 
     @swagger_auto_schema(
@@ -123,14 +114,16 @@ class MyTokenBlacklistView(TokenBlacklistView, InvalidToken):
         operation_description="토큰 블랙리스트",
         responses={ # can use schema or text 
                     200: '로그아웃 성공', 
-                    400: '이미 로그아웃 됨(토큰 블랙리스트)' }
+                    401: '이미 로그아웃 됨(토큰 블랙리스트)' }
     )
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            return Response(serializer.validated_data, status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
 
+        try:
+            serializer.is_valid(raise_exception=True)
+            return Response(data=serializer.validated_data, status=status.HTTP_200_OK)
 
-    
+        except:
+            return Response("Token is invalid or expired", status.HTTP_401_UNAUTHORIZED)
+
+        

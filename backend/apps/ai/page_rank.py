f3006982e7fdfec70f740bcbdb28dcbbf7f7411a
hyeminKim <hmkim199@naver.com>
2022-03-09 17:02:59 +0900

    fix: change hanspell route

--- backend/apps/ai/page_rank.py
+++ backend/apps/ai/page_rank.py
@@ -3,10 +3,9 @@ import traceback
 import typing as ty
 
 import pytube
+from backend.apps.ai.hanspell import spell_checker
 from krwordrank.word import summarize_with_keywords
 from youtube_transcript_api import YouTubeTranscriptApi
-
-from hanspell import spell_checker
 
 sample_url = "https://www.youtube.com/watch?v=mc02IZhouEg"
 sample_url2 = "https://youtu.be/mc02IZhouEg"
